cab5f68e00323a17bf542baff00d3a3aef2c1e0c
ToanTHCS <huongchuongbc1978@gmail.com>
2025-02-01 15:17:04 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import Head from "next/head";
-import 'styles/global.css'; // ✅ Cách thông dụng
+import 'global.css'; // ✅ Cách thông dụng
 
 export default function Home() {
   useEffect(() => {
